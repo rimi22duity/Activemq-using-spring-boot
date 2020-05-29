944e09f17ac610ed178cfe75554538b063eeede4
Rimi Reza <rimi.duity22@gmail.com>
2020-05-30 03:54:50 +0600

    Testing ActiveMQ with a simple project

---
+++ activemq-sender/src/test/java/com/duity/activemqsender/ActivemqSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duity.activemqsender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivemqSenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
